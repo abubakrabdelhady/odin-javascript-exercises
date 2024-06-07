6a8a5dbab096ea5343811e47dff57cef04f23dac
Abubakr Abdelhady <171250585+abubakrabdelhady@users.noreply.github.com>
2024-06-07 15:19:01 +0300

    Solve ex. 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeats) {
+    if(repeats<0) return 'ERROR';
+    let finalText = '';
+    for(let r = 0; r < repeats; r++) {
+        finalText += text;
+    }
+    return finalText;
 };
 
 // Do not edit below this line
