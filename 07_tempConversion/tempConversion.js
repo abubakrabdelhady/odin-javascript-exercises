a5d334137f075d12f454fd4d85644b30092e7de6
Abubakr Abdelhady <171250585+abubakrabdelhady@users.noreply.github.com>
2024-06-07 16:17:09 +0300

    Solve ex. 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const toOneDecimal = function(number) {
+  return Math.round(number * 10) / 10;
+}
+const convertToCelsius = function(fah) {
+  return toOneDecimal((fah - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return toOneDecimal((cel * ( 9 / 5)) + 32);
 };
 
 // Do not edit below this line
