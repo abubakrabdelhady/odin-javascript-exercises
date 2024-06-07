1adeb3a71bacddc7f114a7d675a0777892279e0b
Abubakr Abdelhady <171250585+abubakrabdelhady@users.noreply.github.com>
2024-06-07 16:06:23 +0300

    Solve ex. 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divByFour = (year%4) === 0;
+    const divByHundred = (year%100) === 0;
+    const divByFourHundred = (year%400) === 0;
 
+    return divByFour && (!divByHundred || divByFourHundred);
 };
 
 // Do not edit below this line
