86e84932718b55f2f674cee5a7cce5b0e8adb244
Abubakr Abdelhady <171250585+abubakrabdelhady@users.noreply.github.com>
2024-06-07 15:53:34 +0300

    Solve ex. 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    // make sure both are integers and positive
+    if(!Number.isInteger(first) || 
+        !Number.isInteger(second) || 
+        first < 0 || 
+        second < 0) {
+        return 'ERROR';
+    }
+    
+    // consider may be integers are not in order
+    const inOrder = first <= second;
+    let start = inOrder ? first : second;
+    let end = inOrder ? second : first;
+    
+    // do summition
+    let sum = 0;
+    for(let n = start; n <= end; n++) {
+        sum += n;
+    }
+    return sum;
 };
 
 // Do not edit below this line
